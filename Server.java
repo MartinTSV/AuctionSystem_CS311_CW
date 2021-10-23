6bb985c210067d7e1e5dfb10d1c9e90bc19b3486
unknown <m.tsvetanov@lancaster.ac.uk>
2021-10-23 19:30:39 +0300

    second commit

--- Server.java
+++ Server.java
@@ -1,1 +1,21 @@
-hi
+public class Client {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: java Client n");
+            return;
+        }
+        int n = Integer.parseInt(args[0]);
+
+        try {
+            String name = "myserver";
+            Registry registry = LocateRegistry.getRegistry("localhost");
+            ICalc server = (ICalc) registry.lookup(name);
+
+            int result = server.factorial(n);
+            System.out.println("result is " + result);
+        } catch (Exception e) {
+            System.err.println("Exception:");
+            e.printStackTrace();
+        }
+    }
+}
