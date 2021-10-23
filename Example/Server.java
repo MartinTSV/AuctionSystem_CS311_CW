50704e43dedca1279b07188b130f8ea6ad0c4aed
unknown <m.tsvetanov@lancaster.ac.uk>
2021-10-23 19:27:34 +0300

    first commit

---
+++ Example/Server.java
@@ -0,0 +1,33 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server implements ICalc {
+    public Server() {
+        super();
+    }
+
+    public int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        System.out.println("client request handled");
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Server s = new Server();
+            String name = "myserver";
+            ICalc stub = (ICalc) UnicastRemoteObject.exportObject(s, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(name, stub);
+            System.out.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Exception:");
+            e.printStackTrace();
+        }
+    }
+}
